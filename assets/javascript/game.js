8d0925d5cf92f791d4a2979e8fc6c7aa63371634
Kristen Truyol <kristentruyol@Kristens-MacBook-Air.local>
2018-07-20 17:16:43 -0400

    added images and basic html content

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
-//create 4 crystals as buttons
+//create array of 4 crystals as buttons
+
+//create a for loop to go through each crystal button
+   
+    //create a variable name for jquery button name
+    //then give it a class
+    //append to the div
+
+    //then create on-click
+    //computer assigns a value at random for each crystal and hides this info, options are 1-12
+    //when clicked, score is updated
 
 //score starts at 0
 
@@ -7,13 +17,12 @@
 //computer chooses a number at random and displays it on the screen for user
     //options are 19-120
 
-//computer chooses a value for each crystal at random but doesn't display it
-    //options are 1-12
-
-//when user clicks crystal, points will be added to score
-
 //if user score matches computer choice, user wins
 
 //if user score is higher than computer choice, user loses
 
 //game restarts with the win or loss
+
+// give a number value to each crystal button, then the computer can randomly change the value via java/jquery
+    // for html <button id="red-crystal" class="btn btn-primary number
+    //then in java/jquery $(number) val()
