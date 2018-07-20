13d8e24560677bbd6c5dd58a4af50c188597e984
Kristen Truyol <kristentruyol@Kristens-Air.centurylink.net>
2018-07-20 08:04:40 -0400

    pseudo coding started

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//create 4 crystals as buttons
+
+//score starts at 0
+
+//show user wins and user losses
+
+//computer chooses a number at random and displays it on the screen for user
+    //options are 19-120
+
+//computer chooses a value for each crystal at random but doesn't display it
+    //options are 1-12
+
+//when user clicks crystal, points will be added to score
+
+//if user score matches computer choice, user wins
+
+//if user score is higher than computer choice, user loses
+
+//game restarts with the win or loss
