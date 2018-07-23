f5c5f550432ef69e1f808a548d963d730fea8e23
Kristen Truyol <kristentruyol@Kristens-MacBook-Air.local>
2018-07-23 09:33:52 -0400

    fixed webpage title and still attempting the fix for wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,107 +8,131 @@ var greenGemBtn = [Math.floor(Math.random()* 11) +1];
 console.log("green gem is " + greenGemBtn);
 var purpleGemBtn = [Math.floor(Math.random()* 11) +1];
 console.log("purple gem is " + purpleGemBtn);
-//for current score
-var currentScore = 0;
-$("#currentscore").text(currentScore);
-console.log("current score is " + currentScore);
+
 //for computer's choice of score
 var computerChoiceTarget = [Math.floor(Math.random()* 101) +19];
 //prints target score
 $("#targetscore").text(computerChoiceTarget);
 console.log("target score is " + computerChoiceTarget);
 //for totals
-var wins = 0;
-$("#wins").text(wins);
-console.log("number of wins is " + wins);
+
 var losses = 0;
-$("#losses").text(losses);
+$("#totallosses").text(losses);
 console.log("number of losses is " + losses);
-
-
-function lose() {
-	losses ++;
-	$("#losses").text(losses);
-};
-function win() {
-	wins ++;
-	$("#wins").text(wins);
-};
-
-    //then create on-click
-    //when clicked, score is updated
+var wins = 0;
+$("#totalwins").text(wins);
+console.log("number of wins is " + wins);
+//for current score
+var currentScore = 0;
+$("#currentscore").text(currentScore);
+console.log("current score is " + currentScore);
+//functions for adding win or loss point
+// function win() {
+// 	wins ++;
+// 	$("#totalwins").text(wins);
+// }
+// function lose() {
+// 	losses ++;
+// 	$("#totallosses").text(losses);
+// }
+    //on-click for each crystal
+    //when clicked, current score is updated
 
 $("#red-gem").on("click", function(){
     currentScore = parseInt(currentScore) + parseInt(redGemBtn);
     $("#currentscore").text(currentScore);
     console.log("new user score is " + currentScore);
-    if (computerChoiceTarget === currentScore){
+    if (computerChoiceTarget < currentScore) {
+        //$("#losses").text(losses);
+        lose();
+        console.log("user loses");
+    }
+    else if (computerChoiceTarget === currentScore) {
         // $("#wins").text(wins);
         win();
     console.log("user wins ");
     
     }
-    else {
-        // $("#losses").text(losses);
-        lose();
-        console.log("user loses");
-    }
+    
 });
 
 $("#blue-gem").on("click", function(){
     currentScore = parseInt(currentScore) + parseInt(blueGemBtn);
     $("#currentscore").text(currentScore);
     console.log("new user score is " + currentScore);
-    if (computerChoiceTarget === currentScore){
-        // $("#wins").text(wins);
+    if (computerChoiceTarget < currentScore) {
+        //$("#losses").text(losses);
+        lose();
+        console.log("user loses");
+    }
+    else if (computerChoiceTarget === currentScore) {
+        //$("#wins").text(wins);
         win();
     console.log("user wins ");
     
     }
-    else {
-        // $("#losses").text(losses);
-        lose();
-        console.log("user loses");
-    }
+    
 });
 $("#green-gem").on("click", function(){
     currentScore = parseInt(currentScore) + parseInt(greenGemBtn);
     $("#currentscore").text(currentScore);
     console.log("new user score is " + currentScore);
-    if (computerChoiceTarget === currentScore){
-        // $("#wins").text(wins);
+    
+    if (computerChoiceTarget < currentScore) {
+       // $("#losses").text(losses);
+        lose();
+        console.log("user loses");
+    }
+    else if (computerChoiceTarget === currentScore) {
+        //$("#wins").text(wins);
         win();
     console.log("user wins ");
     
-    }
-    else {
-        // $("#losses").text(losses);
-        lose();
-        console.log("user loses");
     }
 });
 $("#purple-gem").on("click", function(){
     currentScore = parseInt(currentScore) + parseInt(purpleGemBtn);
     $("#currentscore").text(currentScore);
     console.log("new user score is " + currentScore);
-    if (computerChoiceTarget === currentScore){
-        // $("#wins").text(wins);
+    if (computerChoiceTarget < currentScore){
+        //$("#losses").text(losses);
+        lose();
+        updatePage();
+        console.log("user loses");
+    }
+    else if (computerChoiceTarget === currentScore) {
+        //$("#wins").text(wins);
         win();
+        updatePage();
     console.log("user wins ");
     
     }
-    else {
-        // $("#losses").text(losses);
-        lose();
-        console.log("user loses");
-    }
+    
 });
-//if user score matches computer choice, user wins
+function lose() {
+	losses ++;
+    $("#totallosses").text(losses);
+    updatePage();
+};
+function win() {
+	wins ++;
+    $("#totalwins").text(wins);
+    updatePage();
+}
 
+// //if user score matches computer choice, user wins
+// if (computerChoiceTarget === currentScore) {
+//     // $("#wins").text(wins);
+//     win();
+// console.log("user wins ");
 
-
-
-
+// }
+// //if user score is higher than computer choice, user loses
+// else if (computerChoiceTarget < currentScore){
+//     // $("#losses").text(losses);
+//     lose();
+//     console.log("user loses");
+// }
 
 //game restarts with the win or loss
 
